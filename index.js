fab2792885ecfc4999142360170132554c4b5d8d
mitchelle.js <mitchellenjoki7@gmail.com>
2023-07-12 21:13:38 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+function multiply () {
+    return num1 * num2
+}
+console.log(1 * 62)
+
+function random () {
+    // return  random.isInteger
+    return Math.random
+}
+
+console.log (Math.random())
+function mod () {
+    return num4 % num5
+}
+console.log(85 % 9)
+
+const max = Math.max (2,6,16,20)
